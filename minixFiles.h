08fd2390ca7bb6446b2eb2adcff1ae69e77c0435
Miles Chaffin <mileschaffin@gmail.com>
2018-12-02 23:20:42 -0800

    wrote parser

--- minixFiles.h
+++ minixFiles.h
@@ -2,8 +2,17 @@
 #define MINIXFILES_H
 
 #include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define FALSE 0
+#define TRUE 1
 
 #define DIRECT_ZONES 7
+
+struct fsinfo parser(int argc, const char * argv[], int get);
 
 struct partitionTable {
     uint8_t bootind;
@@ -23,34 +32,45 @@ struct superblock { /* Minix Version 3 Superblock
                      * in minix 3.1.1
                      */
     /* on disk. These fields and orientation are non–negotiable */
-    uint32 t ninodes; /* number of inodes in this filesystem */
-    uint16 t pad1; /* make things line up properly */
-    int16 t i blocks; /* # of blocks used by inode bit map */
-    int16 t z blocks; /* # of blocks used by zone bit map */
-    uint16 t firstdata; /* number of first data zone */
-    int16 t log zone size; /* log2 of blocks per zone */
-    int16 t pad2; /* make things line up again */
-    uint32 t max file; /* maximum file size */
-    uint32 t zones; /* number of zones on disk */
-    int16 t magic; /* magic number */
-    int16 t pad3; /* make things line up again */
-    uint16 t blocksize; /* block size in bytes */
-    uint8 t subversion; /* filesystem sub–version */
+    uint32_t ninodes; /* number of inodes in this filesystem */
+    uint16_t pad1; /* make things line up properly */
+    int16_t i_blocks; /* # of blocks used by inode bit map */
+    int16_t z_blocks; /* # of blocks used by zone bit map */
+    uint16_t firstdata; /* number of first data zone */
+    int16_t log_zone_size; /* log2 of blocks per zone */
+    int16_t pad2; /* make things line up again */
+    uint32_t max_file; /* maximum file size */
+    uint32_t zones; /* number of zones on disk */
+    int16_t magic; /* magic number */
+    int16_t pad3; /* make things line up again */
+    uint16_t blocksize; /* block size in bytes */
+    uint8_t subversion; /* filesystem sub–version */
 };
 
 struct inode {
-    uint16 t mode; /* mode */
-    uint16 t links; /* number or links */
-    uint16 t uid;
-    uint16 t gid;
-    uint32 t size;
-    int32 t atime;
-    int32 t mtime;
-    int32 t ctime;
-    uint32 t zone[DIRECT ZONES];
-    uint32 t indirect;
-    uint32 t two indirect;
-    uint32 t unused;
+    uint16_t mode; /* mode */
+    uint16_t links; /* number or links */
+    uint16_t uid;
+    uint16_t gid;
+    uint32_t size;
+    int32_t atime;
+    int32_t mtime;
+    int32_t ctime;
+    uint32_t zone[DIRECT_ZONES];
+    uint32_t indirect;
+    uint32_t two_indirect;
+    uint32_t unused;
+};
+
+struct fsinfo {
+    char * imagefile;
+    char * filepath;
+    char * srcpath;
+    char * dstpath;
+    int part;
+    int subpart;
+    int verbose;
+
 };
 
 
---
+++ minls.c
@@ -0,0 +1,10 @@
+#include "minixFiles.h"
+
+int VERBOSE = FALSE;
+
+int main(int argc, const char* argv[]){
+    
+    struct fsinfo fs = parser(argc, argv, FALSE);
+
+}
+
